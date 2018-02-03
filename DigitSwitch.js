51658b26296708ae9d9c90d6f40c1cb890c988e1
ljutrk <ljubomir.trkulja88@gmail.com>
2018-02-03 10:15:37 +0100

    Initial change

--- DigitSwitch.js
+++ DigitSwitch.js
@@ -1,10 +1,9 @@
-var a = 25;
-var result = "";
-var digit;
+var a = 52;
 
 if (typeof a === "number") {
     if (a / 10 >= 1 && a / 10 < 10) {
-        digit = a % 10;
+        var digit = a % 10;
+        var result = "";
         result += digit;
         digit = (a - digit) / 10;
         result += digit;

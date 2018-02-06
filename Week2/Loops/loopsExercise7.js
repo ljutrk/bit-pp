239c340022208ab6c95d502b4adedb4547c9b8ac
BIT Student <classroom@bgit.rs>
2018-02-06 17:57:59 +0100

    final

--- Week2/Loops/loopsExercise7.js
+++ Week2/Loops/loopsExercise7.js
@@ -7,11 +7,18 @@ for (var i = 0; i < arr.length; i++) {
 
 }
 avg = sum / arr.length;
+console.log(avg);
 if (avg < 60) {
     console.log("F");
+} else if (avg < 70) {
+    console.log("D");
+} else if (avg < 80) {
+    console.log("C");
+} else if (avg < 90){
+    console.log("B");
+} else if (avg < 100){
+    console.log("A");
 } else {
     console.log("You failed the test!");
 }
 
-    
-}
